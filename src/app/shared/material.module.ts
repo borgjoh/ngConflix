ff13d38a0096ff84f9a92550a4a7c3ec737bdcb4
Johann Borg <borgjoh@gmail.com>
2018-04-24 23:35:37 +0200

    Material set-up

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material';
+
+const modules = [
+  MatButtonModule
+];
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [modules],
+  exports: [modules]
 })
 export class MaterialModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from './material.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   declarations: []
 })
